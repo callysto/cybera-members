c111aa7d720c751620d339c8295095d11e8669a9
David Hay <misterhay@gmail.com>
2023-07-04 21:43:42 -0600

    Add try except WiFi connection

--- micropython/main.py
+++ micropython/main.py
@@ -9,24 +9,32 @@ led.on()
 passwords = [['SSID','password']]
 
 # connect the network
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(passwords[0][0], passwords[0][1])
-wait = 10
-while wait > 0:
-    if wlan.status() < 0 or wlan.status() >= 3:
-        break
-    wait -= 1
-    print('waiting for connection...')
-    sleep(1)
-# Handle connection error
-if wlan.status() != 3:
-    raise RuntimeError('WiFi connection failed')
-    import cybera
-    cybera
-else:
-    ip = wlan.ifconfig()[0]
-    print('IP: ', ip)
+try:
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(passwords[0][0], passwords[0][1])
+    wait = 10
+    while wait > 0:
+        if wlan.status() < 0 or wlan.status() >= 3:
+            break
+        wait -= 1
+        print('waiting for connection...')
+        sleep(1)
+    # Handle connection error
+    if wlan.status() != 3:
+        raise RuntimeError('WiFi connection failed')
+        import cybera
+        cybera
+    else:
+        ip = wlan.ifconfig()[0]
+        print('IP: ', ip)
+        led.off()
+except:
+    for _ in range(4):
+        led.off()
+        sleep(0.3)
+        led.on()
+        sleep(0.3)
     led.off()
 
 
